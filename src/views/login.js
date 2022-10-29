ca49c5748eca3f8a698ba0e10e7db3e9bdb1cdc9
AyodejiSheen <aoluwasegun009@gmail.com>
2022-10-29 22:21:14 +0100

    latest

--- src/views/login.js
+++ src/views/login.js
@@ -10,6 +10,9 @@ import AuthContext from '../context/auth/context';
 import UiContext from '../context/UI/context';
 
 
+import google from '../assets/media/Google__G__Logo.svg.png'
+import twitter from '../assets/media/Twitter-logo.svg.png'
+
 
 
 
@@ -61,7 +64,7 @@ export const Login = () => {
         <>
 
             <section className='bg-gray-100 flex flex-wrap xl:h-screen w-screen m-0 p-8 justify-center'>
-                
+
                 <div className='lg:m-auto my-6'>
                     <div className='space-y-3'>
                         <div>
@@ -75,7 +78,7 @@ export const Login = () => {
 
 
 
-                <div className='m-auto shadow-lg bg-white  p-8 md:p-14 xl:p-20 rounded-2xl space-y-8 w-full md:w-4/5 lg:w-7/12  xl:w-1/2 2xl:w-1/3'>
+                <div className='m-auto  bg-white  p-8 md:p-14 xl:p-20 rounded-2xl space-y-8 w-full md:w-4/5 lg:w-7/12  xl:w-1/2 2xl:w-1/3'>
                     <div className='space-y-2'>
                         <h2 className='text-xl font-extrabold text-center'> Sign In</h2>
                         <p className='font-medium text-center text-slate-400'>Sign in to your account</p>
@@ -87,7 +90,7 @@ export const Login = () => {
                             <a href="https://tweetclips-test.herokuapp.com/api/v1/googleoauth"
                                 className="flex items-center justify-center w-full px-4 py-2.5 text-sm font-medium leading-5 text-gray-700 transition-colors duration-150 border border-gray-300 rounded-lg dark:text-gray-400 active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:outline-none focus:shadow-outline-gray"
                             >
-                                <i className="lni lni-google mr-3"></i>
+                                <img src={google} alt="img" className="w-4 mr-3" />
                                 Sign in with Google
                             </a>
                         </div>
@@ -96,7 +99,7 @@ export const Login = () => {
                             <a href="nothing"
                                 className="flex items-center justify-center w-full px-4 py-2.5 text-sm font-medium leading-5 text-gray-700 transition-colors duration-150 border border-gray-300 rounded-lg dark:text-gray-400 active:bg-transparent hover:border-gray-500 focus:border-gray-500 active:text-gray-500 focus:outline-none focus:shadow-outline-gray"
                             >
-                                <i class="lni lni-twitter-original mr-3"></i>
+                                <img src={twitter} alt="img" className="w-4 mr-3" />
                                 Sign in with Twitter
                             </a>
                         </div>
