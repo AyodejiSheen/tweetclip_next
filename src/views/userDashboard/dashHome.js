22f0f16fe1a72ac9631790774fc68c7ecd5034bf
AyodejiSheen <aoluwasegun009@gmail.com>
2022-11-16 23:25:10 +0100

    new

--- src/views/userDashboard/dashHome.js
+++ src/views/userDashboard/dashHome.js
@@ -1,7 +1,7 @@
 import Logo from '../../assets/media/logo.png'
 import { useContext, useEffect } from 'react'
 import UiContext from '../../context/UI/context'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import { Theme } from '../../components/theme'
 import { ProfileNav } from '../../components/profileNav'
 import AuthContext from '../../context/auth/context'
@@ -10,6 +10,7 @@ import { Artboard } from './artboard'
 import { LinkImport } from '../../components/linkImport'
 import ArtBoardContext from '../../context/artboard/context'
 import { Controls } from './controls'
+import { AllProjects } from './allProject'
 
 
 
@@ -17,6 +18,7 @@ import { Controls } from './controls'
 export const DashHome = () => {
 
     let { isDark, show, showItem } = useContext(UiContext)
+    const navigate = useNavigate();
 
 
     const { loadUsersDetails, loading, isLoading, user } = useContext(AuthContext)
@@ -38,22 +40,29 @@ export const DashHome = () => {
                 loading ? */}
 
 
-                    <section className='flex flex-col lg:flex-row p-5 lg:p-10 items-start bg-neutral-50 dark:bg-slate-900 gap-y-6 lg:gap-y-0'>
-                        <div className='fixed w-1/4 px-8 inset-y-24 dark:bg-slate-800  bg-neutral-100 rounded-xl lg:rounded-[1.8rem] h-full'>
-                            <Controls />
-                        </div>
+            <section className='lg:flex items-center lg:flex-row p-5 lg:p-10 bg-neutral-50 dark:bg-slate-900 gap-y-6 lg:gap-y-0'>
 
-                        <div className=''>
-                            {
-                                show === "import" && (
-                                    <LinkImport />
-                                )
-                            }
-                            <Artboard />
-                        </div>
-                    </section>
+                <div className='lg:fixed lg:w-72 xl:w-1/4 px-8 inset-y-16 mt-16 md:mt-20 h-96 lg:h-auto dark:bg-slate-800  bg-slate-100 rounded-xl lg:rounded-2xl xl:rounded-[1.8rem] flex'>
+                    <div className='m-auto space-y-6 flex flex-col justify-center'>
+                        <div className='mx-auto rounded-full flex items-center justify-center w-36 h-36 lg:w-24 lg:h-24 xl:w-36 xl:h-36 bg-blue-500 text-white text-6xl font-medium lg:text-3xl'><i class="lni lni-download"></i></div>
 
-                    {/* :
+                        <div className='mx-auto'>
+                            <button onClick={() => showItem('import')} className="mx-auto  font-medium bg-emerald-200 text-green-600 dark:text-green-400 hover:dark:bg-green-700 dark:bg-emerald-900 px-5 py-2 rounded-lg hover:bg-green-500 dark:hover:text-white hover:text-white lg:text-sm xl:text-base">Import Link</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='lg:flex-1 xl:w-1/4 lg:ml-80 xl:ml-[28%] my-8 lg:my-20 h-screen'>
+                    {
+                        show === "import" && (
+                            <LinkImport />
+                        )
+                    }
+                    <AllProjects />
+                </div>
+            </section>
+
+            {/* :
 
                     Loading.standard()
             } */}
--- src/views/userDashboard/dashboardNav.js
+++ src/views/userDashboard/dashboardNav.js
@@ -7,6 +7,7 @@ import { ProfileNav } from '../../components/profileNav'
 import AuthContext from '../../context/auth/context'
 import { Artboard } from './artboard'
 import { LinkImport } from '../../components/linkImport'
+import dp from '../../assets/media/dp.png'
 
 
 
@@ -27,40 +28,39 @@ export const DashboardNav = () => {
 
                 <section className='bg-neutral-50 dark:bg-slate-900 h-screen'>
 
-                    <div className='flex flex-wrap justify-between px-32 fixed w-full h-20 py-3 bg-neutral-50 z-50'>
-                        <div className='w-1/3 '>
-                            <img src={Logo} className="w-64" alt='tweeclip-logo' />
+                    <div className='flex justify-between items-center px-4 md:px-6 lg:px-12 2xl:px-32 fixed w-full h-16 md:h-20 lg:h-24 py-3 lg:py-4 bg-neutral-100 dark:bg-slate-900 lg:gap-16 z-50'>
+                        <div className='lg:w-1/5 xl:w-1/4'>
+                            <img src={Logo} className="w-32 object-contain xl:w-64 md:w-44" alt='tweeclip-logo' />
                         </div>
 
-                        <div className='w-2/3 '>
-                            <div className='flex flex-col lg:flex-row lg:justify-between lg:items-center gap-y-6 lg:gap-y-0'>
-                                <div className='flex gap-2 order-2 lg:order-1 justify-center '>
-                                    <div className='cursor-pointer border-2 dark:border-slate-600 dark:text-slate-300 px-6 md:px-10 py-3 flex items-center justify-center rounded-xl space-x-2 text-sm md:text-base '>
+                        <div className='lg:w-4/5 xl:w-2/3'>
+                            <div className='flex justify-between items-center'>
+                                <div className='hidden lg:flex gap-2 justify-center '>
+                                    <div className='cursor-pointer border-2 dark:border-slate-600 dark:text-slate-300 px-6 xl:px-10 py-3 flex items-center justify-center rounded-xl space-x-2 text-sm  xl:text-base'>
                                         <i className="lni lni-save"></i>
                                         <p>Save</p>
                                     </div>
 
-                                    <div onClick={() => showItem('import')} className='cursor-pointer border-2 dark:border-slate-600 dark:text-slate-300 px-6 md:px-10 py-3 flex items-center justify-center rounded-xl space-x-2 text-sm md:text-base '>
+                                    <div onClick={() => showItem('import')} className='cursor-pointer border-2 dark:border-slate-600 dark:text-slate-300 px-6 xl:px-10 py-3 flex items-center justify-center rounded-xl space-x-2 text-sm  xl:text-base'>
                                         <i className="lni lni-cloud-download"></i>
                                         <p>Import</p>
                                     </div>
 
-                                    <div className='cursor-pointer lg:hidden border-2 dark:border-slate-600 dark:text-slate-300 px-6 md:px-10 py-3 flex items-center justify-center rounded-xl space-x-2 text-sm md:text-base '>
+                                    <div className='cursor-pointer lg:hidden border-2 dark:border-slate-600 dark:text-slate-300 px-6 xl:px-10 py-3 flex items-center justify-center rounded-xl space-x-2 text-sm  xl:text-base '>
                                         <i className="lni lni-cloud-upload"></i>
                                         <p>Export</p>
                                     </div>
                                 </div>
 
-                                <div className='flex gap-4 lg:gap-6 order-1 lg:order-2 items-center'>
-
+                                <div className='flex gap-2  md:gap-6  items-center'>
                                     <div className="flex  items-center flex-1 ">
-                                        <div className="relative lg:w-80  focus-within:text-sky-500">
+                                        <div className="relative w-32 md:w-80 lg:w-64 focus-within:text-sky-500">
                                             <div className="absolute inset-y-0 flex items-center pl-3">
                                                 <svg className="w-4 h-4 dark:text-slate-300" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                                                     <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
                                                 </svg>
                                             </div>
-                                            <input className="w-full pl-8 pr-2 py-2 h-10 md:h-14 text-sm text-gray-700 placeholder-gray-600 bg-gray-100 border-0 rounded-xl dark:placeholder-gray-500 dark:focus:shadow-outline-gray dark:focus:placeholder-gray-600 dark:bg-slate-700 dark:text-slate-300 focus:placeholder-gray-500 focus:bg-white focus:border-purple-300 focus:outline-none focus:shadow-outline-purple form-input" type="text" placeholder="Search for projects" aria-label="Search" />
+                                            <input className="w-full pl-8 pr-2 py-2 h-10 md:h-14  text-xs md:text-sm text-gray-700 placeholder-gray-600 bg-gray-100 border-0 rounded-xl dark:placeholder-gray-500 dark:focus:shadow-outline-gray dark:focus:placeholder-gray-600 dark:bg-slate-700 dark:text-slate-300 focus:placeholder-gray-500 focus:bg-white focus:border-purple-300 focus:outline-none focus:shadow-outline-purple form-input" type="text" placeholder="Search for projects" aria-label="Search" />
                                         </div>
                                     </div>
 
@@ -71,20 +71,22 @@ export const DashboardNav = () => {
 
 
                                     <div>
+                                        <img src={dp} className=" cursor-pointer w-10 lg:w-14 rounded-xl" alt='img' onClick={() => showItem('profile')} />
 
                                         {/* {
+
                                             !isLoading && user.displayPic !== null ? (
                                                 <img src={`${user.displayPic.imageUrl}`} className=" cursor-pointer w-10 lg:w-14 rounded-xl" alt='img' onClick={() => showItem('profile')} />
                                             ) : (
                                                 <div className='cursor-pointer text-white bg-blue-500 dark:text-slate-300  w-10 h-10 lg:h-14 lg:w-14 flex items-center justify-center hover:text-blue-400 rounded-xl hover:dark:text-blue-400 text-2xl font-bold' onClick={() => showItem('profile')} >{user.email.charAt(0).toUpperCase()}</div>
                                             )
-                                        }
+                                        }*/}
 
                                         {
                                             show === "profile" && (
                                                 <ProfileNav />
                                             )
-                                        } */}
+                                        }
                                     </div>
                                 </div>
                             </div>
@@ -92,7 +94,7 @@ export const DashboardNav = () => {
                     </div>
 
                     <div className=''>
-                        <Outlet/>
+                        <Outlet />
                     </div>
 
                 </section>
