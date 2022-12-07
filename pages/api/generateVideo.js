d35c27e6e738830630300cc454a6bb27aa5c55c4
AyodejiSheen <aoluwasegun009@gmail.com>
2022-12-07 17:16:58 +0100

    new

--- pages/api/generateVideo.js
+++ pages/api/generateVideo.js
@@ -6,28 +6,37 @@ import { deploySite, getOrCreateBucket } from "@remotion/lambda";
 import { baseUrl } from "../../baseUrl";
 
 export default async function handler(req, res) {
+  console.log(req.body)
   const { composition, siteName } = JSON.parse(req.body);
-  // Get the bucket name
-  const { bucketName } = await getOrCreateBucket({
-    region: "us-east-1",
-  });
 
-  // Deploy latest version of composition
-  const { serveUrl } = await deploySite({
-    bucketName,
-    entryPoint: path.resolve(process.cwd(), "remotion/index.js"),
-    region: "us-east-1",
-    siteName, // Pass the artboard siteName here
-  });
+  try {
+    // Get the bucket name
+    const { bucketName } = await getOrCreateBucket({
+      region: "us-east-1",
+    });
 
-  // Generate video
-  const reponse = await axios.post(
-    `${baseUrl}/generate-video`,
-    {
-      composition,
-      serveUrl,
-    }
-  );
-  console.log(reponse.data);
-  res.status(200).json({ name: serveUrl });
+    // Deploy latest version of composition
+    const { serveUrl } = await deploySite({
+      bucketName,
+      entryPoint: path.resolve(process.cwd(), "remotion/index.js"),
+      region: "us-east-1",
+      siteName, // Pass the artboard siteName here
+    });
+
+    // Generate video
+    const reponse = await axios.post(`${baseUrl}/generate-video`,
+      {
+        composition,
+        serveUrl,
+      }
+    );
+
+    // console.log(reponse.data);
+    res.status(200).json({ name: serveUrl });
+
+  } catch (error) {
+    console.log(error)
+  }
+
 }
+
